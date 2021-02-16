6b34b3b9e996983298dc71d1f62a44f7f438e203
Iliya Akimov <i.akimov@wis.eu.com>
2021-02-16 17:32:23 +0300

    feat: add card to sign in form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
@@ -21,6 +22,7 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatCardModule,
 
     AuthRoutingModule,
   ]
