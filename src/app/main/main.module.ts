93f4cc0d7ecb5fe8c064ff855a592a24649b872f
Iliya Akimov <i.akimov@wis.eu.com>
2021-02-18 10:06:22 +0300

    feat: add navbar component

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainContainer } from './containers/main/main.container';
 import { MainRoutingModule } from './main-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
 @NgModule({
-  declarations: [MainContainer],
+  declarations: [MainContainer, NavbarComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
   ]
 })
 export class MainModule { }
